5cf5026323d1dcc13e8bc65c5eecf89919189ae2
Magdiel Burdiles <magdielmbm@gmail.com>
2026-09-15 02:31:01 -0300

    Cambios en la entidad, para que traiga los datos

--- src/main/java/com/docente/proyectoejemplo/mascotas/entity/Mascota.java
+++ src/main/java/com/docente/proyectoejemplo/mascotas/entity/Mascota.java
@@ -2,23 +2,28 @@ package com.docente.proyectoejemplo.mascotas.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
 
 @Entity
+@Table (name="MASCOTAS")
 public class Mascota {
     @Id 
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Short id;
 
-    @Column
+    @Column(name = "edad_mascota")
     private Short edad;
 
-    @Column
+    @Column(name= "nombre_mascota")
     private String nombre;
 
-    @Column
+    @Column(name= "vacunado")
     private boolean vacunado;
 
-    @Column
+    @Column(name= "especie")
     private String especie;
 
     public Short getId() {

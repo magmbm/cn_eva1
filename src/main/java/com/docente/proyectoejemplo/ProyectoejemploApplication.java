e0f49982059f0efb154c19580ed5a1a3269eaa30
Magdiel Burdiles <magdielmbm@gmail.com>
2026-09-15 01:14:54 -0300

    Escaneo de entidades cambiado

--- src/main/java/com/docente/proyectoejemplo/ProyectoejemploApplication.java
+++ src/main/java/com/docente/proyectoejemplo/ProyectoejemploApplication.java
@@ -2,8 +2,10 @@ package com.docente.proyectoejemplo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.docente.proyectoejemplo.Entidades")
 public class ProyectoejemploApplication {
 
     public static void main(String[] args) {

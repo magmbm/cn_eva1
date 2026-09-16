7d9c9466a05a24dfdf472d97f9034cb08d192df6
Magdiel Burdiles <magdielmbm@gmail.com>
2026-09-16 01:38:35 -0300

    imports no ocupados ahora eliminados

--- src/main/java/com/docente/proyectoejemplo/mascotas/MascotaController.java
+++ src/main/java/com/docente/proyectoejemplo/mascotas/MascotaController.java
@@ -1,9 +1,7 @@
 package com.docente.proyectoejemplo.mascotas;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 
--- src/main/java/com/docente/proyectoejemplo/mascotas/MascotaService.java
+++ src/main/java/com/docente/proyectoejemplo/mascotas/MascotaService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 
 import com.docente.proyectoejemplo.mascotas.entity.Mascota;
 
-import java.lang.foreign.Linker.Option;
 import java.util.List;
 import java.util.Optional;
 
